9b626a43c526b8f1d42e3c2b68f5311c0f6ce020
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2022-10-06 19:50:31 +0300

    Added stat counters

--- ivTimer/src/renderer/views/Home.vue
+++ ivTimer/src/renderer/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <main class="timerbox">
     <calendar/>
+    <section class="timerbox__section">
+    <p class="timerbox__counters">
+      Общее количество задач: <span>{{totalCount}}</span>
+    </p>
+    <p class="timerbox__counters">
+      Количество задач на сегодня: <span>{{currentDayCount}}</span>
+    </p>
+    <p class="timerbox__counters">
+      Количество задач на месяц: <span>{{currentMonth}}</span>
+    </p>
+    </section>
   </main>
 </template>
 <script>
@@ -9,8 +20,31 @@ export default {
   name: 'Home',
   components: {
     calendar
+  },
+  computed: {
+    totalCount () {
+      return this.$store.getters.totalTasksCount
+    },
+    currentDayCount () {
+      return this.$store.getters.currentDayList.length
+    },
+    currentMonth () {
+      return this.$store.getters.currentMonthCount
+    }
   }
 }
 </script>
 <style scoped lang="scss">
+.timerbox {
+  &__section {
+    text-align: center;
+  } 
+  &__counters {
+    color: white;
+    span {
+      color: yellow;
+    }
+  }
+
+}
 </style>
